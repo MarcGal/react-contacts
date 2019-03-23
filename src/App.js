950830703d347261852e4d141853f6670efd53a7
Margal <mgalloperalta@gmail.com>
2019-03-23 13:38:31 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,76 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Contact from './components/Contact';
-import contacts from './data/contacts.json'
+import contacts from './data/contacts.json';
+import { sortByName } from './helpers';
+import { sortByPop } from './helpers';
+
 import './App.css';
 
-// const fiveContacts = contacts.slice(0, 5);
+// const myContacts = contacts.slice(0, 5);
 
 class App extends Component {
 
   state = {
-    fiveContacts: contacts.slice(0,5),
+    myContacts: contacts.slice(0,5),
   }
 
   addRandomContact = (event) =>{
     const lengthContacts = contacts.length;
     const randomContact = contacts[Math.floor(Math.random() * lengthContacts)];
     this.setState({
-      fiveContacts:[...this.state.fiveContacts,randomContact]
+      myContacts: [...this.state.myContacts,randomContact]
     })
   }
+
+  sortAlpha = (event) =>{
+    //Siempre es mejor crear una copia a partir del array original
+    const orderedContacts = [...this.state.myContacts]
+    //Separamos la funcion sortByName en otro archivo
+    orderedContacts.sort(sortByName)
+    this.setState({
+      myContacts: orderedContacts
+    }, () => {
+      //this.seState tiene un callback que podemos uilizar
+      console.log(this.state.myContacts)  
+    })
+  }
+
+  sortPop = (event) => {
+    const orderedContacts = [...this.state.myContacts]
+    orderedContacts.sort(sortByPop)
+    this.setState({
+      myContacts: orderedContacts
+    }, () => {
+      //this.seState tiene un callback que podemos uilizar
+      console.log(this.state.myContacts)  
+    })
+  }
+
+  delete = (index) => {
+    console.log(index);
+    this.state.myContacts.splice(index, 1);
+    console.log(this.state);
+    this.setState({
+      myContacts: [...this.state.myContacts],
+    })
+  }
+
   render() {
-    console.log(this.state)
     return (
       <div className="App">
+        <h1>IronContacts</h1>
         <button onClick={this.addRandomContact}>Add random contact</button>
+        <button onClick={this.sortAlpha}>Sort by name</button>
+        <button onClick={this.sortPop}>Sort by popularity</button>
         <ul>
-          { this.state.fiveContacts.map((contact, index) => {
+          { this.state.myContacts.map((contact, index) => {
             return < Contact 
             img= { contact.pictureUrl} 
             name={contact.name} 
             key= {index}
             popularity= {contact.popularity}
+            deleteContact = { this.delete }
           />
           }) }
         </ul>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 
 class Contact extends Component {
+
+  handleDelete = (e) => {
+    this.props.deleteContact(this.props.index);
+  }
+
   render() {
     const { img, name, popularity} = this.props
     return (
@@ -9,6 +14,7 @@ class Contact extends Component {
          <img className="img" src={ img } alt=""/>
          <p>{ name }</p>
          <p>{ popularity}</p>
+         <button onClick={this.handleDelete}>Delete</button>
        </li>
     )
   }
